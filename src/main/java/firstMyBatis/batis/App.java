d1faab8631b7802162200de785170776f12e747a
Kuen <Kuen@LKF-DELL1>
2019-06-09 20:06:59 +0800

    mybatis

---
+++ src/main/java/firstMyBatis/batis/App.java
@@ -0,0 +1,41 @@
+package firstMyBatis.batis;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import firstMyBatis.batis.po.City;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) throws IOException {
+		System.out.println(System.getProperty("user.dir"));
+		
+		
+		
+		String resPath = "/SqlMapConfig.xml";
+		File f = new File(resPath);
+		FileInputStream is = new FileInputStream(f);
+		byte b[] = new byte[512];
+		is.read(b, 0, 512);
+		System.out.println(new String(b));
+		
+//		Reader reader = Resources.getResourceAsReader(resPath);
+//		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+//		SqlSession sqlSession = sqlSessionFactory.openSession();
+//		City city = sqlSession.selectOne("City.findCityByID", 1);
+//		System.out.println(city.toString());
+//		sqlSession.close();
+		
+	}
+}
---
+++ src/main/java/firstMyBatis/batis/po/City.java
@@ -0,0 +1,48 @@
+package firstMyBatis.batis.po;
+
+public class City {
+
+	public String id;
+	public String name;
+	public String countryCode;
+	public String district;
+	public String population;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCountryCode() {
+		return countryCode;
+	}
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+	public String getDistrict() {
+		return district;
+	}
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+	public String getPopulation() {
+		return population;
+	}
+	public void setPopulation(String population) {
+		this.population = population;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString()+"   "+this.name;
+	}
+
+}
